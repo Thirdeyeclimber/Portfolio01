ace90226d6c6ccbbe2f7c84ba329811a7863b482
Michael Day <mr.enviro@gmail.com>
2021-02-01 19:09:45 -0700

    adds js calculator to projects

--- app.js
+++ app.js
@@ -1,31 +1,157 @@
 // Slide show for core trainer project presentation
 var slideIndex = 1;
-			showSlides(slideIndex);
+showSlides(slideIndex);
 
-			// Next/previous controls
-			function plusSlides(n) {
-				showSlides(slideIndex += n);
-			}
+// Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
 
-			// Thumbnail image controls
-			function currentSlide(n) {
-				showSlides(slideIndex = n);
-			}
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
 
-			function showSlides(n) {
-				var i;
-				var slides = document.getElementsByClassName("mySlides")
-				var dots = document.getElementsByClassName("demo");
-				var captionText = document.getElementById("caption");
-				if (n > slides.length) {slideIndex = 1}
-				if (n < 1) {slideIndex = slides.length}
-				for (i = 0; i < slides.length; i++) {
-					slides[i].style.display = "none";
-				}
-				for (i = 0; i < dots.length; i++) {
-					dots[i].className = dots[i].className.replace(" active", "");
-				}
-				slides[slideIndex-1].style.display = "block";
-				dots[slideIndex-1].className += " active";
-				captionText.innerHTML = dots[slideIndex-1].alt;
-			}
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides")
+	var dots = document.getElementsByClassName("demo");
+	var captionText = document.getElementById("caption");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+// CALCULATOR FUNCTIONALITY
+class Calculator {
+	constructor(previousOperandTextElement, currentOperandTextElement) {
+		this.previousOperandTextElement = previousOperandTextElement
+		this.currentOperandTextElement = currentOperandTextElement
+		this.clear()
+	}
+
+	clear() {
+		this.currentOperand = ''
+		this.previousOperand = ''
+		this.operation = undefined
+	}
+	
+	delete() {
+		this.currentOperand = this.currentOperand.toString().slice(0, -1)
+	}
+
+	appendNumber(number) {
+		if (number === '.' && this.currentOperand.includes('.')) return
+		this.currentOperand = this.currentOperand.toString() + number.toString()
+	}
+	
+	chooseOperation(operation) {
+		if (this.currentOperand === '') return
+		if (this.previousOperand !== '') {
+			this.compute()
+		}
+		this.operation = operation
+		this.previousOperand = this.currentOperand
+		this.currentOperand = ''
+	}
+	
+	compute() {
+		let computation
+		const prev = parseFloat(this.previousOperand)
+		const current = parseFloat(this.currentOperand)
+		if (isNaN(prev) || isNaN(current)) return
+		switch (this.operation) {
+			case '+':
+				computation = prev + current
+				break
+			case '-':
+				computation = prev - current
+				break
+			case '*':
+				computation = prev * current
+				break
+			case '÷':
+				computation = prev / current
+				break
+			default:
+				return
+		}
+		this.currentOperand = computation
+		this.operation = undefined
+		this.previousOperand = ''
+	}
+	
+	getDisplayNumber(number) {
+		const stringNumber = number.toString()
+		const integerDigits = parseFloat(stringNumber.split('.')[0])
+		const decimalDigits = stringNumber.split('.')[1]
+		let integerDisplay
+		if (isNaN(integerDigits)) {
+			integerDisplay = ''
+		} else {
+			integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+		}
+		if (decimalDigits != null) {
+			return `${integerDisplay}.${decimalDigits}`
+		} else {
+			return integerDisplay
+		}
+	}
+
+	updateDisplay() {
+		this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+		if (this.operation != null) {
+			this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+		} else {
+			this.previousOperandTextElement.innerText = ''
+		}
+	}
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		calculator.appendNumber(button.innerText)
+		calculator.updateDisplay()
+	})
+})
+
+operationButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		calculator.chooseOperation(button.innerText)
+		calculator.updateDisplay()
+	})
+})
+
+equalsButton.addEventListener('click', button => {
+	calculator.compute()
+	calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+	calculator.clear()
+	calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+	calculator.delete()
+	calculator.updateDisplay()
+})
